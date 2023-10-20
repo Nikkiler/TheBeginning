fc4833d245117a9d50bc52ccda3ecdf3f785650d
Nikita <kit.lobanov@icloud.com>
2023-10-21 12:43:49 +1300

    Added chat functionality

--- Client.py
+++ Client.py
@@ -1,17 +1,38 @@
 import socketio
+
 sio = socketio.Client()
+
 
 @sio.event
 def connect():
     print('connection established')
+
+
 @sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+def Message(data):
+    print('message: ', data)
+
 
 @sio.event
 def disconnect():
     print('disconnected from server')
+
+
+@sio.event
+def menu():
+    print("1. Login")
+    print("2. Send a message")
+    choice = int(input())
+    while choice != 1 and choice != 2:
+        print("Choose 1 or 2")
+        choice = int(input())
+    if choice == 1:
+        print('Feature Unavailable at the moment')
+    elif choice == 2:
+        print('What would you like to send to the server?')
+        message = input()
+        sio.emit('Message', message)
+
 
 sio.connect('http://localhost:5001')
 sio.wait()
--- Websocket.py
+++ Websocket.py
@@ -1,12 +1,29 @@
 import eventlet
 import socketio
+
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
+
 @sio.event
 def connect(sid, environ):
     print('connect ', sid)
+    sio.enter_room(sid, 'Chat')
+    sio.emit('menu')
+
+
+@sio.event
+def Message(sid, data):
+    print('message ', data)
+    sio.emit('Message', data, room='Chat', skip_sid=sid)
+
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5001)), app)
